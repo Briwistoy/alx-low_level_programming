019805123bdb5a7a407fd103b8b20a085618defc
Briwistoy <efekpokpomonday@gmail.com>
2023-07-16 14:34:40 -0700

    all

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Always 0 (success)
+ */
+int main(void){
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	Printf("size of a int: %d byte(s)\n", sizeof(int));
+	Printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	Printf("size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
